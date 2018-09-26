14ad18ac979bc43331184b687ef35af13ec92ab8
samueljosh <samueljoshua@Live.com>
2018-09-26 20:28:41 -0300

    Create projeto.py

---
+++ projeto.py
@@ -0,0 +1,14 @@
+# ProjetoPA
+import numpy as np
+import cv2
+
+img = cv2.imread('messi5.jpg',0)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
